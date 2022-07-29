f76d24bd54ef847fd5c0d56f966e5fb29c9c1ef3
SimonDMaxwell <lolicongress@outlook.com>
2022-07-28 20:58:28 -0500

    TEAR DOWN

--- api-backend/server/server10.js
+++
@@ -1,6 +0,0 @@
-const app = require('../app10');
-// 3) START SERVER
-const port = 3000;
-app.listen(port, ()=>{
-  console.log(`Server Listening on Port ${port}...`);
-})
--- api-backend/server2.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app-9');
-// 3) START SERVER
-const port = 3000;
-app.listen(port, ()=>{
-  console.log(`Server Listening on Port ${port}...`);
-})
